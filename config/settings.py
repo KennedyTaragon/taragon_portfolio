1b55ed210bd869eab060c28b2f9407ccf3ed3767
Vascokenn <kennedykoech048@gmail.com>
2025-10-15 09:05:56 +0300

    Cleaned json junk

--- config/settings.py
+++ config/settings.py
@@ -39,11 +39,26 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'cloudinary_storage',  # Add this BEFORE django.contrib.staticfiles
+    'cloudinary',        
     'apps.portfolio',
     'ckeditor',
     'django_recaptcha',
     'whitenoise',
 ]
+
+import cloudinary
+import cloudinary.uploader
+import cloudinary.api
+
+CLOUDINARY_STORAGE = {
+    'CLOUD_NAME': config('CLOUDINARY_CLOUD_NAME'),
+    'API_KEY': config('CLOUDINARY_API_KEY'),
+    'API_SECRET': config('CLOUDINARY_API_SECRET'),
+}
+
+# Update media file storage
+DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 
 MIDDLEWARE = [
     'django.middleware.gzip.GZipMiddleware',
